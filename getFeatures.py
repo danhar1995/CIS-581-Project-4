137620e6b1fb974728796d680da40be8f0e14025
zouenhui <zoue999@gmail.com>
2017-11-21 17:24:26 -0500

    getFeature Rough Draft

--- getFeatures.py
+++ getFeatures.py
@@ -15,4 +15,57 @@
 
 def getFeatures(img, bbox):
   #TODO: Your code here
-  return x, y
+    import numpy as np
+    import cv2
+    import detectFace
+    import scipy
+    from skimage.feature import corner_shi_tomasi,corner_harris
+    #img=cv2.imread('many.jpg')
+    #bbox=detectFace.detectFace(img)
+    gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    [row,col,dim]=np.asarray(bbox.shape)
+    bbox=bbox.astype(int)
+    neighbors=np.ones((3,3))
+    xcoord=[]
+    ycoord=[]
+    numPt=0
+    for i in range(row):
+        currentFace=gray[bbox[i,0,1]:bbox[i,2,1],bbox[i,0,0]:bbox[i,1,0]]
+        currentFace=corner_harris(currentFace)
+        idx=(currentFace>0.5)
+        currentFace=currentFace*idx
+        localM=scipy.ndimage.filters.maximum_filter(currentFace,footprint=neighbors)
+        msk=(currentFace==localM)
+        msk=msk*currentFace
+        [y,x]=np.where(msk>0)
+        x=x+bbox[i,0,0]
+        y=y+bbox[i,0,1]
+        if len(x)>numPt:
+            numPt=len(x)
+        xcoord.append(x)
+        ycoord.append(x)
+    #    for k in range(len(x)):
+    #        cv2.circle(gray,(x[k],y[k]),5,(0,0,255),1)
+    x=np.ones((numPt,row))*-1
+    y=x
+    print numPt
+    for k in range(row):
+        x[0:len(xcoord[k]),k]=xcoord[k]
+        y[0:len(xcoord[k]),k]=ycoord[k]
+    return x,y
+#cv2.imshow('fig1',gray)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+#gray=corner_harris(gray) #harris corner detection
+#grayidx=(gray>0.35) #threshold suppression index
+#gray=gray*grayidx #filter out values less than threshold
+#neighbors=np.ones((3,3)) #define neighbors for local max sup.
+#localM=scipy.ndimage.filters.maximum_filter(gray,footprint=neighbors)#find value of local max
+#msk=(gray==localM) #find boolean map
+#msk=msk*gray#compute new gray image 
+#tf=np.where(msk>0)
+#cv2.imshow('fig1',msk)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+#  return x, y
